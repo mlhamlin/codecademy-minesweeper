6111260c40f1fe44fbc03df5380b90cfc9671411
Martha Hamlin <mlhamlin92@gmail.com>
2018-12-08 13:28:02 -0500

    minesweeper part 3, loops and higher order functions

--- minesweeper.js
+++ minesweeper.js
@@ -1,19 +1,47 @@
-const printBoard = (board) => {
-    console.log('Current Board:');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    const board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
 };
 
-let board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    const board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        const row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
 
-printBoard(board);
+    let numberOfBombsPlaced = 0;
 
-board[0][1] = '1';
-board[2][2] = 'B';
+    while (numberOfBombsPlaced < numberOfBombs) {
+        //Note this code can place bombs over other bombs resulting in less bombs total
+        // this will be fixed later
+        const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
 
-printBoard(board);
+    return board;
+};
+
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+const playerBoard = generatePlayerBoard(3, 4);
+const bombBoard = generateBombBoard(3, 4, 5);
+
+console.log("Player Board: ");
+printBoard(playerBoard);
+console.log("Bomb Board: ");
+printBoard(bombBoard);
